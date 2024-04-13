f96ce6ca084bba0c1278b1af9db5ef17aae0b2d3
Rafael Lima Paulo <rlprafa@gmail.com>
2024-04-13 14:37:26 -0400

    finalizando o serviço do modelo de produção

--- code/application.py
+++ code/application.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 import numpy as np
 
@@ -8,6 +10,8 @@ import mlflow
 from mlflow.models.signature import infer_signature
 from mlflow.tracking import MlflowClient
 
+from sklearn.metrics import confusion_matrix, classification_report
+import seaborn as sns
 
 
 
@@ -24,6 +28,46 @@ mlflow.set_experiment(experiment_name)
 
 colunas = ['lat','lon','minutes_remaining', 'period', 'playoffs', 'shot_distance']
 
+def plot_matriz_confusao(data_prod):
+    # Remove linhas com NaN na coluna 'shot_made_flag'
+    data_clean = data_prod.dropna(subset=['shot_made_flag'])
+    
+    # Convertendo os escores de previsão em classes preditas
+    predicted_classes = (data_clean['predict_score'] > 0.5).astype(int)
+    
+    cm = confusion_matrix(data_clean['shot_made_flag'], predicted_classes)
+    
+    plot.figure(figsize=(10, 8))
+    sns.heatmap(cm, annot=True, fmt="d")
+    plot.title('Matriz de Confusão - Modelo de Produção')
+    plot.xlabel('Predito')
+    plot.ylabel('Verdadeiro')
+
+    image = plot.gcf()
+    image_name = 'matriz_confusao_producao.png'
+    image.savefig(image_name, dpi=100)
+
+    #plot.show()
+    plot.close()
+    return image_name
+
+def plot_desempenho(data_prod):
+    # Remove linhas com NaN na coluna 'shot_made_flag'
+    data_clean = data_prod.dropna(subset=['shot_made_flag'])
+    
+    # Cálculo das métricas de desempenho na base de produção
+    predicted_classes = (data_clean['predict_score'] > 0.5).astype(int)
+    report = classification_report(data_clean['shot_made_flag'], predicted_classes, output_dict=True)
+    # Convertendo o dicionário em um DataFrame para facilitar a visualização e o log
+    report_df = pd.DataFrame(report).transpose()
+    print(report_df)
+    # Salvando o DataFrame como um arquivo CSV
+    report_csv = 'classification_report.csv'
+    report_df.to_csv(report_csv, index=True)
+    return report_csv
+
+    
+
 with mlflow.start_run(experiment_id=experiment_id, run_name = 'PipelineAplicacao'):
 
     model_uri = f"models:/modelo_kobe@staging"
@@ -35,6 +79,18 @@ with mlflow.start_run(experiment_id=experiment_id, run_name = 'PipelineAplicacao
     prediction_file = '../data/processed/prediction_prod.parquet'
     data_prod.to_parquet(prediction_file)
     mlflow.log_artifact(prediction_file)
+    
+    # executa o plot da matriz de confusão e loga no mlflow
+    image_name = plot_matriz_confusao(data_prod)
+    mlflow.log_artifact(image_name)
+
+    os.remove(image_name)
+    
+    # imprime o desempenho com os dados de peoducao
+    report_name = plot_desempenho(data_prod)
+    mlflow.log_artifact(report_name)
+    os.remove(report_name)
+    
     
     print(data_prod)
     
